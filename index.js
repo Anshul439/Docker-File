ba4dab1e56a52c70b74d9cb9a750982425210b3f
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-07-21 04:43:24 +0530

    Add some files

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
+const express = require('express');
+const app = express()
 
-mongoose.connect("mongodb://127.0.0.1:27017/", {
-    dbName: "backend",
-})
-    .then(() => console.log("Database Connected"))
-    .catch((e) => console.log(e));
+const PORT = process.env.PORT || 5000
 
-    const messageSchema = new mongoose.Schema({
-        name: String,
-        email: String,
-    });
-
-    const Message = mongoose.model("Message", messageSchema);
-
-const app = express();
-
-// Using Middlewares
-app.use(express.static(path.join(path.resolve(), "public")));
-app.use(express.urlencoded({ extended: true }))
-
-// Setting up view engine
-app.set("view engine", "ejs");
-
-app.get('/', (req, res) => {
-    res.render("index")
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello Docker" })
 })
 
-// app.get("/add", async (req, res) => {
-//     await Message.create({name: "Anshul", email: "diamond439msn@gmail.com"});
-//     res.send("Nice");
-// });
-
-app.get('/success', (req, res) => {
-    res.render("success");
-})
-
-app.post('/contact', async (req, res) => {
-
-    const {name, email} = req.body;
-
-    await Message.create({ name: name, email: email});
-    // res.render("success");
-    res.redirect("/success");
-})
-
-app.get("/users", (req, res) => {
-    res.json({
-        users,
-    })
-})
-
-app.listen(5000, () => {
-    console.log("Server is running");
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`)
 })
